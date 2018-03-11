f529f7b60a56c60fb9da7d9744ec446e073b24bc
Quentin JEROME <qjerome@users.noreply.github.com>
2018-03-11 23:32:56 +0100

    trial.ps1

---
+++ trial.ps1
@@ -0,0 +1,20 @@
+$version = "v1.1"
+$url = "https://github.com/0xrawsec/whids/releases/download/$version"
+$arch = $ENV:PROCESSOR_ARCHITECTURE
+$bin = "$PWD\"+"whids.exe"
+$remoteBinBase = "whids-$version-"
+
+If ( $arch -eq "AMD64" ) {
+  $url = $url + "/$remoteBinBase" + "amd64.exe"
+} else {
+  $url = $url + "/$remoteBinBase" + "386.exe"
+}
+
+Write-Output "Downloading: $url"
+Write-Output "WHIDS Path: $bin"
+[Net.ServicePointManager]::SecurityProtocol = "tls12"
+$client = New-Object System.Net.WebClient
+$client.DownloadFile($url, $bin)
+
+& $bin -v
+& $bin -t 1 -u -c "security,sysmon"
